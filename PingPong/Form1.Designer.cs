91def52305f42dff723f7a6c8e08a55f7458ca18
S A N D <S A N D@SANDRAS_PC>
2024-08-05 18:48:30 +0100

    Add project files.

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.Computer = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.player.Location = new System.Drawing.Point(58, 76);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 120);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Computer
+            // 
+            this.Computer.BackColor = System.Drawing.Color.Green;
+            this.Computer.Location = new System.Drawing.Point(602, 75);
+            this.Computer.Name = "Computer";
+            this.Computer.Size = new System.Drawing.Size(30, 120);
+            this.Computer.TabIndex = 1;
+            this.Computer.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Ball.Location = new System.Drawing.Point(307, 165);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(30, 30);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Computer);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Player : 0 -- Computer: 0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox Computer;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+
+        int ballXSpeed = 4; //ball speed on X axis
+        int ballYSpeed = 4; //ball speed on Y axis
+        int speed = 2; //computer speed
+        int playerspeed = 8;  //player speed
+
+        //IMPORTANT!!!!!!!
+        Random rand = new Random(); //for selecting random speed for ball and computer
+
+
+        bool goDown, goUp; //controls when player moves up or down 
+        int computer_speed_change = 50; //interval for how often computer speed changes to d next random speed
+        int playerScore = 0; //scores of player
+        int computerScore = 0; //scores of computer
+        int[] i = { 5, 6, 8, 9 }; //the random numbers for computer speed will be taken from here, and assigned to "int speed = 2"
+        int[] j = {10, 9, 8, 11, 12};//random numbers for ballXSpeed & ballYspeed will be taken from here
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            Ball.Top -= ballYSpeed;
+            Ball.Left -= ballXSpeed;
+
+            this.Text = "Player Score: " + playerScore + "-- Computer Score: " + computerScore;
+
+            if(Ball.Top < 0 || Ball.Bottom > this.ClientSize.Height) //if the ball has reached the top || bottom of the screen 
+            {
+                ballYSpeed = -ballYSpeed; //ballYspeed should reverse and go opposite direction
+
+            } 
+            if(Ball.Left < -2)
+            {
+                Ball.Left = 300; //reset the ball back to 300
+                ballXSpeed = -ballXSpeed; //move ball in opposite direction 
+                computerScore++; //add 1 to the computer score (computer WON)
+            }
+            if(Ball.Right > this.ClientSize.Width + 2)
+            {
+                Ball.Left = 300;
+                ballXSpeed = -ballXSpeed;
+                playerScore++; //add 1 to the player's score (Player WON)
+            }
+
+            if(Computer.Top <= 1)
+            {
+                Computer.Top = 0; //reset to 0
+            }
+            else if(Computer.Bottom >= this.ClientSize.Height) 
+            {
+                Computer.Top = this.ClientSize.Height - Computer.Height; //computer.Height is the height of d computer picture box
+
+            }
+
+            if(Ball.Top < Computer.Top + (Computer.Height / 2 ) && Ball.Left > 300)
+            {//Computer.Top + (Computer.Height / 2) will make the computer follow the ball from the center of the computer paddle
+                
+                Computer.Top -= speed; 
+            }
+            if(Ball.Top > Computer.Top + (Computer.Height / 2 ) && Ball.Left > 300)
+            {
+                Computer.Top += speed; //increase the speed
+            }
+
+            computer_speed_change -= 1; //reducing computer speed change by 1
+
+            if(computer_speed_change < 0)
+            {
+                speed = i[rand.Next(i.Length)];
+                computer_speed_change = 50;
+
+            }
+
+            if(goDown && player.Top + player.Height < this.ClientSize.Height) //if goDown is true and player is still within the height of the ball
+            {
+                player.Top += playerspeed; //get the player to continue to move down till it reaches the bottom
+            }
+
+            if(goUp && player.Top > 0) 
+            {
+                player.Top -= playerspeed;  //move the player to the top by reducing the player speed
+            }
+
+            //checking collission by passing the ball in pic1, player in pic2, and offset value is playerRight + 5
+            CheckCollision(Ball, player, player.Right + 5);
+
+            //checking collission by passing the ball in pic1, computer in pic2, and offset value is computerLeft - 35
+            CheckCollision(Ball, Computer, Computer.Left - 35); 
+
+            if(computerScore > 5)
+            {
+                GameOver("You Lost");
+            }
+            else if(playerScore > 5)
+            {
+                GameOver("You WON!!");
+            }
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e) //pressing down a key
+        {
+            if(e.KeyCode == Keys.Down) //if the down key is pressed
+            {
+                goDown = true; //make the goDown bool to be true
+            }
+            if(e.KeyCode == Keys.Up) //if the up key is pressed
+            {
+                goUp = true; //make the goUp bool to be true
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e) //not pressing down a key(Key is Up)
+        {
+            if(e.KeyCode == Keys.Down) //if down key is released
+            {
+                goDown = false; //goDown bool will be false
+            }
+            if(e.KeyCode == Keys.Up) //if up key is released
+            {
+                goUp = false;  //goUp bool will be false
+            }
+        }
+
+
+
+        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))//if PicOne is colliding with PicTwo
+            {
+                PicOne.Left = offset;
+                int x = j[rand.Next(j.Length)]; //select one of the random values from the j arrays and assign to x
+                int y = j[rand.Next(j.Length)];
+
+                if (ballXSpeed < 0) 
+                {
+                    ballXSpeed = x; //set the number from negative to positive (x stands for any random number from j array)
+                }
+                else
+                {
+                    ballXSpeed = -x; //set the number from positive to negative(move the ball from right to left)
+                }
+
+                if (ballYSpeed < 0) //if Y value is negative(less than 0)
+                {
+                    ballYSpeed = -y; //new value should also be negative
+                }
+                else
+                {
+                    ballYSpeed = y; //new value should be positive(faster or slower depending on the random array number)
+                }
+            }
+        }
+
+
+        private void GameOver(string message)
+        {
+            GameTimer.Stop();
+            MessageBox.Show(message, "Moo says: ");
+            computerScore = 0;
+            playerScore = 0;
+            ballXSpeed = ballYSpeed = 4;
+            computer_speed_change = 50;
+            GameTimer.Start();
+        }
+
+    }
+}
